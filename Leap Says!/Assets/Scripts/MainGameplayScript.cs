1ac33ecb261fed556bcdd6296c476cf9f14ba96b
Achanigo <alejandrofederico@hotmail.com>
2013-10-05 03:42:41 -0600

    Added more foleys, sign textures & textures

---
+++ Leap Says!/Assets/Scripts/MainGameplayScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainGameplayScript : MonoBehaviour 
+{
+	public GameObject[] signArray;
+	public GUITexture[] signTextures;
+	private int[] numberMatrix;
+	private int currentIndex;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		this.numberMatrix = new int[18];
+		this.currentIndex = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
